734e8f38be910a772cc70c1735a2e7cbc1a76c24
刘某某 <2071708612@qq.com>
2019-07-06 15:47:34 +0800

    首页-侧边栏样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,95 @@
 <template>
-    <div class="home">
-        登陆成功
-    </div>
+  <div class="home">
+    <el-container>
+      <el-aside width="200px">
+        <div class="logo"></div>
+        <el-menu
+          default-active="1-1"
+          class="el-menu-admin"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 <script>
-import {getUserList} from '@/api/index.js'
+import { getUserList } from "@/api/index.js";
 export default {
-    mounted () {
-        let params = {params: {query: '', pagenum: 1, pagesize:  1}}
-        getUserList(params).then(res => {
-            window.console.log(res)
-        })
+  mounted() {
+    let params = { params: { query: "", pagenum: 1, pagesize: 1 } };
+    getUserList(params).then(res => {
+      window.console.log(res);
+    });
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      window.console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      window.console.log(key, keyPath);
     }
-}
+  }
+};
 </script>
 <style lang="scss" scoped>
-
+.home {
+  height: 100%;
+  .el-menu-admin:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .el-container {
+    height: 100%;
+  }
+  .el-aside {
+    background-color: #545c64;
+    .el-menu {
+      border-right: solid 0px #e6e6e6;
+    }
+  }
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #545c64;
+  }
+  .logo {
+    height: 60px;
+    background: url(../assets/logo01.jpg);
+    background-size: cover;
+    background-color: #545c64;
+  }
+  .toggle-btn {
+    font-size: 36px;
+    color: #989898;
+    cursor: pointer;
+    line-height: 60px;
+  }
+  .system-title {
+    font-size: 28px;
+    color: white;
+  }
+  .welcome {
+    color: white;
+  }
+}
 </style>
