57c176ae8f3cca3662a4288ee7477d26473b7f58
刘某某 <2071708612@qq.com>
2019-07-06 14:48:04 +0800

    路由守卫完成

--- src/main.js
+++ src/main.js
@@ -9,6 +9,23 @@ import '@/styles/index.scss'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
+//注册一个全局守卫，作用是在路由跳转前，对路由进行判断，防止未登陆的用户跳转到其他页面去
+router.beforeEach((to, from, next) => {
+  let token = localStorage.getItem('mytoken')
+  //如果已经登陆，那么就不干涉
+  if (token) {
+    next()
+  } else {
+    if (to.path !== '/login') {
+      //如果没有登陆就跳转到登陆页
+      next({path: '/login'})
+    } else {
+      //如果没有登陆，但是访问的是login，不干涉
+      next()
+    }
+  }
+})
+
 new Vue({
   el: '#app',
   router,
